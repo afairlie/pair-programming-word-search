63b792e76fb82e750e354d36a9abee000773c7d6
afairlie <ariane.fairlie@gmail.com>
2020-04-08 21:50:53 +0000

    write additional tests for  edge cases and comlete function - pair programmed with Amro

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,86 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    // column 6, rows 1-7;
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'R', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'O', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'G', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'R', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'M', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'PROGRAM')
+
+    assert.isTrue(result);
+  });
+
+  it ("should return false for empty array", () => {
+    const input = wordSearch([], 'PROGRAM');
+    assert.isFalse(input);
+  })
+  it ("should return false if empty string provided as word param", () => {
+    const input = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'R', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'O', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'G', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'R', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'M', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+    assert.isFalse(input);
+  });
+
+  it ("should return false if no word param provided", () => {
+    const input = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'R', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'O', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'G', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'R', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'M', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ]);
+    assert.isFalse(input);
+  });
+
+  it ("should return true even if array input lowercase", () => {
+    const input = wordSearch([
+      ['a', 'w', 'c', 'f', 'q', 'u', 'a', 'l'],
+      ['s', 'e', 'i', 'n', 'f', 'e', 'p', 'd'],
+      ['y', 'f', 'c', 'f', 'q', 'u', 'r', 'l'],
+      ['h', 'm', 'j', 't', 'e', 'v', 'o', 'g'],
+      ['w', 'h', 'c', 's', 'y', 'e', 'g', 'l'],
+      ['b', 'f', 'r', 'e', 'n', 'e', 'r', 'b'],
+      ['u', 'b', 't', 'w', 'a', 'p', 'a', 'i'],
+      ['o', 'd', 'c', 'a', 'k', 'u', 'm', 's'],
+      ['e', 'z', 'k', 'f', 'q', 'u', 'a', 'l'],
+    ], 'PROGRAM')
+    assert.isTrue(input);
+  });
+  
+  it("should return true if array input is lowercase and search word is uppercase", () =>{
+    const input = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'R', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'O', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'G', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'R', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'M', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'program');
+
+    assert.isTrue(input);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,50 @@
+// pair programmed by ariane and amro git: @afairlie, @amrhamada
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters.length === 0 || !word) {
+        return false;
     }
+    
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    for (l of horizontalJoin) {
+        if (l.toUpperCase().includes(word.toUpperCase())) return true
+    }
+    
+    const transposedLetters = transpose(letters);
+
+    const verticalJoin = transposedLetters.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.toUpperCase().includes(word.toUpperCase())) return true
+    }
+    
+    return false;
 }
+
+const transpose = function(matrix) {
+    let result = [];
+
+    if ( matrix.length === 1 && Array.isArray(matrix[0])){
+        for ( const [index, num] of matrix[0].entries()){
+        result[index] = [num];
+        }
+        return result;
+    }
+
+    let count = 0;
+
+    while (count < matrix[0].length){
+        result[count] = [];
+        count++;
+    }
+
+    for (const [rowIndex, val] of matrix.entries()) {
+        for (const [columnIndex, element] of matrix[rowIndex].entries()) {     
+        result[columnIndex][rowIndex] = element;   
+        }
+    }
+
+    return result;
+
+};
 
 module.exports = wordSearch
